840092adb0262ccbb672a20b12a63817dda5ce1c
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2024-02-02 09:50:55 +0530

    manifest changed

--- store-admin/frontend/src/config/SERVER_URL.js
+++ store-admin/frontend/src/config/SERVER_URL.js
@@ -1,6 +1,6 @@
 // const SERVER_URL = "http://localhost:3000/api"
 // const SERVER_URL = "https://award-store.onrender.com/api"
-const SERVER_URL = "http://dccstore-admin.sanjaysanthosh.me/api"
+const SERVER_URL = "http://dccstore.sanjaysanthosh.me/api"
 
 
 export default SERVER_URL;
